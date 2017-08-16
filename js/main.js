cd2bfd40f7049754dd554129c6d0614fe66f9cc7
frank <2900410568@qq.com>
2017-08-16 17:48:24 +0800

    首页基本完成

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$("#blog-warp")
+var blogs = {};
+
+$.ajax({
+    url:"https://raw.githubusercontent.com/ching2016/MyWebsite/master/data/data.json",
+    dataType: 'json',
+    success:function(data){
+        blogs = data;
+        mekeList(blogs);
+    }
+})
+
+function mekeList(data){
+    // console.log(data);
+    let content = "";
+    for(let i = 0;i < data.length;i ++){
+        content = content + `<div class="blog-info">
+                <a href="#" class="title">${data[i].title}</a>
+                <p class="time">${data[i].time}</p> by <p class="author">${data[i].author}</p>
+                </div>`;
+    }
+    $(".pagination-warp").before(content);
+    // console.log("内容："+content);
+    
+}
