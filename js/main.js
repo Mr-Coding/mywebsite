bb0deba6bd5b20f802806672345dec3968291410
frank <2900410568@qq.com>
2017-08-17 14:38:13 +0800

    完善导航栏

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-var blogs = {};
-
+//ajax和json配合生成文章列表
 $.ajax({
     url:"https://raw.githubusercontent.com/ching2016/MyWebsite/master/data/data.json",
     dataType: 'json',
     success:function(data){
-        blogs = data;
-        mekeList(blogs);
+        mekeList(data);
     }
 })
-
 function mekeList(data){
     let content = "";
     for(let i = 0;i < data.length;i ++){
@@ -19,3 +16,15 @@ function mekeList(data){
     }
     $(".pagination-warp").before(content);
 }
+
+//监听滚动事件，滚动等于或超过100px就给导航添加颜色，否则透明
+$(window).scroll(function(e){
+    let scrollY = $(window).scrollTop();
+
+    if(scrollY >= 100){
+        $(".changeColor").css({"background-color":"#1F617B"});
+    }else{
+        $(".changeColor").css({"background-color":"rgba(0, 0, 0, .0)"});
+    }
+
+})
