7c1c3946efa85e32e6ca68bf727ff83ab9a2cfc0
frank <2900410568@qq.com>
2017-08-17 20:00:54 +0800

    添加标签样式

--- js/main.js
+++ js/main.js
@@ -9,12 +9,24 @@ $.ajax({
 function mekeList(data){
     let content = "";
     for(let i = 0;i < data.length;i ++){
-        content = content + `<div class="blog-info">
+        content = content + 
+            `<div class="blog-info">
                 <a href="#" class="title">${data[i].title}</a>
                 <p class="time">${data[i].time}</p> by <p class="author">${data[i].author}</p>
-                </div>`;
+                <p class="tag-wrap">` +
+                    mekeTagList(data,i)+
+                `</p>
+            </div>`;
     }
     $(".pagination-warp").before(content);
+}
+function mekeTagList(data,i){
+    let tagContent = "";
+    for(let j = 0;j < data[i].tag.length;j ++){
+        tagContent = tagContent + 
+            `<a href="#" class="tag">${data[i].tag[j]}</a>`
+    }
+    return tagContent;
 }
 
 //监听滚动事件，滚动等于或超过100px就给导航添加颜色，否则透明
