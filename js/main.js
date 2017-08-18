3e82193de9ca218557d3f28894bfd501ea2c5f47
frank <2900410568@qq.com>
2017-08-19 03:32:46 +0800

    '分页功能制作ing...'

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
+const pageWrap = $(".pagination-warp");
+var dataCount = 0;
+
 //ajax和json配合生成文章列表
 $.ajax({
     url:"https://raw.githubusercontent.com/ching2016/MyWebsite/master/data/data.json",
     dataType: 'json',
     success:function(data){
+        dataCount = data.length;
         mekeList(data);
+        PaginationsFactor(data);
     }
 })
 function mekeList(data){
@@ -28,6 +33,51 @@ function mekeTagList(data,i){
     }
     return tagContent;
 }
+
+// json数据分组，每组pageCount个
+// data:json原数据，pageCount:要分的页数，默认10
+function PaginationsFactor(data,pageCount = 10){
+    const pLength = pages(data.length,pageCount); //页数
+    var j = {};
+    var content = []; //临时接收从原json里拆分出的数据
+    var flag = 0; //
+    for(let n = 0;n <pLength;n ++){
+        for(var i = flag;i < pageCount+flag;i ++){
+            if(data[i] === undefined){ //如果不足pageCount个及检测到undefined就break
+                break;
+            }
+            content.push(data[i]);
+            if(flag === data.length){
+                break;
+            }
+            if(i+1 === pageCount+flag){
+                // console.log("结束...");
+                flag = i+1;  
+                break;      
+            }
+        }
+        j[n+1] = content;
+        content = []; //清空数组
+    }
+    console.log(j);
+    return j;
+}
+
+//计算页数
+function pages(data,pageCount){
+    let count = data/pageCount;
+    let allPages = 0;   //总页数
+    if(data%pageCount){
+        console.log("不能整除 "+count+" "+parseInt(count+1));
+        allPages = parseInt(count+1); //去掉小数点再自加1
+    }else{
+        console.log("能整除 "+count);
+        allPages = count;
+    }
+    return allPages;
+}
+
+
 
 //监听滚动事件，滚动等于或超过100px就给导航添加颜色，否则透明
 $(window).scroll(function(e){
