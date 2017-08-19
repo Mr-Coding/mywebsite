38226271a85dd17e74cb3119f07ff882c5c7a709
frank <2900410568@qq.com>
2017-08-19 20:15:03 +0800

    '已实现简单的翻页功能'

--- js/main.js
+++ js/main.js
@@ -1,46 +1,57 @@
-const pageWrap = $(".pagination-warp");
+const next = $(".next");
+const previous = $(".previous")
 var dataCount = 0;
+var toPages = 1;//记录当前所在页数
+var allPage = 0;//记录总页数
+var jsonToPages = {};//整理过后的json
 
+//------------------------
 //ajax和json配合生成文章列表
 $.ajax({
     url:"https://raw.githubusercontent.com/ching2016/MyWebsite/master/data/data.json",
     dataType: 'json',
     success:function(data){
         dataCount = data.length;
-        mekeList(data);
-        PaginationsFactor(data);
+        // console.log(data);
+        jsonToPages = PaginationsFactor(data,5);
+        mekeList(jsonToPages,toPages);
     }
 })
-function mekeList(data){
+//创建文章目录
+function mekeList(data,toPage){
+    $("div").remove(".blog-info");//清空已有的目录，如果有的话
     let content = "";
-    for(let i = 0;i < data.length;i ++){
+    for(let i = 0;i < data[toPage].length;i ++){
         content = content + 
             `<div class="blog-info">
-                <a href="#" class="title">${data[i].title}</a>
-                <p class="time">${data[i].time}</p> by <p class="author">${data[i].author}</p>
+                <a href="#" class="title">${data[toPage][i].title}</a>
+                <p class="time">${data[toPage][i].time}</p> by <p class="author">${data[toPage][i].author}</p>
                 <p class="tag-wrap">` +
-                    mekeTagList(data,i)+
+                    mekeTagList(data,i,toPage)+
                 `</p>
             </div>`;
     }
     $(".pagination-warp").before(content);
 }
-function mekeTagList(data,i){
+//创建文章目录里的标签
+function mekeTagList(data,i,toPage){
     let tagContent = "";
-    for(let j = 0;j < data[i].tag.length;j ++){
+    for(let j = 0;j < data[toPage][i].tag.length;j ++){
         tagContent = tagContent + 
-            `<a href="#" class="tag">${data[i].tag[j]}</a>`
+            `<a href="#" class="tag">${data[toPage][i].tag[j]}</a>`
     }
     return tagContent;
 }
 
-// json数据分组，每组pageCount个
+//--------------------------------------------
+// json数据分组，每1组pageCount个
 // data:json原数据，pageCount:要分的页数，默认10
 function PaginationsFactor(data,pageCount = 10){
     const pLength = pages(data.length,pageCount); //页数
+    allPage = pLength; //传出总页数...
     var j = {};
     var content = []; //临时接收从原json里拆分出的数据
-    var flag = 0; //
+    var flag = 0; //记录已读取到的目录
     for(let n = 0;n <pLength;n ++){
         for(var i = flag;i < pageCount+flag;i ++){
             if(data[i] === undefined){ //如果不足pageCount个及检测到undefined就break
@@ -57,7 +68,7 @@ function PaginationsFactor(data,pageCount = 10){
             }
         }
         j[n+1] = content;
-        content = []; //清空数组
+        content = []; //清空临时数组
     }
     console.log(j);
     return j;
@@ -68,7 +79,7 @@ function pages(data,pageCount){
     let count = data/pageCount;
     let allPages = 0;   //总页数
     if(data%pageCount){
-        console.log("不能整除 "+count+" "+parseInt(count+1));
+        console.log("不能整除 "+count+", "+parseInt(count+1));
         allPages = parseInt(count+1); //去掉小数点再自加1
     }else{
         console.log("能整除 "+count);
@@ -77,8 +88,28 @@ function pages(data,pageCount){
     return allPages;
 }
 
+//下一页
+next.on("click",function(){
+    console.log("下一页"+allPage);
+    if(toPages != allPage){
+        toPages ++;
+        mekeList(jsonToPages,toPages);
+        console.log(toPages);
+    }
+})
+//上一页
+previous.on("click",function(){
+    console.log("上一页");
+    if(toPages != 1){
+        toPages --;
+        mekeList(jsonToPages,toPages);
+        console.log(toPages);
+    }
+})
 
 
+
+//------------------------------------------------------
 //监听滚动事件，滚动等于或超过100px就给导航添加颜色，否则透明
 $(window).scroll(function(e){
     let scrollY = $(window).scrollTop();
